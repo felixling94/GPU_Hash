7f306fb00cf23c834b9c2ed0accf513deb29eeaa
Felix Lichenerwa <felix.ling94@feliciaux.com>
2024-05-01 11:22:37 +0200

    Update 9p

--- tools/cells_program.cpp
+++ tools/cells_program.cpp
@@ -12,7 +12,7 @@ std::vector<cell<uint32_t>> keyList;
 
 //Erzeuge verschiedene Werte für die Schlüssel und deren Längen zufällig
 template <typename T1, typename T2>
-std::vector<cell<T1,T2>> createCells(size_t cells_size, bool key_same=false){
+std::vector<cell<T1,T2>> createCells(size_t cells_size, bool value_same=false){
     std::vector<cell<T1,T2>> cells_vector;
     cells_vector.reserve(cells_size);
 
@@ -20,14 +20,14 @@ std::vector<cell<T1,T2>> createCells(size_t cells_size, bool key_same=false){
     size_t seed = generator();
     std::mt19937 rnd(seed);
 
-    if (key_same == false){
-        T1 key = (T1) 1;
-        T2 value = (T2) 1; 
+    if (value_same == false){
+        T1 key = 0;
+        T2 value = 0; 
         
         for (size_t i = 0; i < cells_size; i++){
-            cells_vector.push_back(cell<T1,T2>{key, value});
             ++key;
             ++value;
+            cells_vector.push_back(cell<T1,T2>{key, value});
         }
         std::shuffle(cells_vector.begin(), cells_vector.end(), rnd);
         return cells_vector;
@@ -35,12 +35,12 @@ std::vector<cell<T1,T2>> createCells(size_t cells_size, bool key_same=false){
     }else{
         std::uniform_int_distribution<T1> dist(1,cells_size);
 
-        T1 key = dist(rnd); 
-        T2 value = (T2) 1;
+        T1 key = 0; 
+        T2 value = dist(rnd);
         
         for (size_t i = 0; i < cells_size; i++){
+            ++key;
             cells_vector.push_back(cell<T1,T2>{key,value});
-            ++value;
         }
         std::shuffle(cells_vector.begin(), cells_vector.end(), rnd);
         return cells_vector;
@@ -74,7 +74,7 @@ void readFile(char * file_name, size_t key_num){
 
 //Schreibe eine Liste von Schlüsseln und deren Längen auf einer Datei
 template <typename T1, typename T2>
-void writeFile(char * file_name, size_t key_num, bool key_same){
+void writeFile(char * file_name, size_t key_num, bool value_same){
     std::ofstream writefile(file_name, std::ios::out | std::ios::binary);
     
     if (!writefile){
@@ -82,7 +82,7 @@ void writeFile(char * file_name, size_t key_num, bool key_same){
         exit (EXIT_FAILURE);
     }
 
-    keyList = createCells<T1,T2>(key_num,key_same);
+    keyList = createCells<T1,T2>(key_num,value_same);
     cellKeyList = keyList.data();
     
     for (size_t i = 0; i < key_num; i++)
@@ -99,8 +99,8 @@ void writeFile(char * file_name, size_t key_num, bool key_same){
 int main(int argc, char** argv){
     size_t key_num;
     char * file_name;
-    int int_key_same, int_read_write;
-    bool key_same, read_write;
+    int int_value_same, int_read_write;
+    bool value_same, read_write;
     std::string file_name_str;
     
     if(argc < 5){
@@ -111,7 +111,7 @@ int main(int argc, char** argv){
     int_read_write = atoi(argv[1]);
     key_num = (size_t) atoi(argv[2]);
     file_name = argv[3];
-    int_key_same = atoi(argv[4]);
+    int_value_same = atoi(argv[4]);
 
     if (int_read_write<0 || int_read_write>1){
         std::cout << "Der Code des Schreibens oder Lesens von Datei muss entweder 0 bis 1 sein." << std::endl;
@@ -130,7 +130,7 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    if (int_key_same<0 || int_key_same>1){
+    if (int_value_same<0 || int_value_same>1){
         std::cout << "Der Code der Gleichheit der Schlüsselgröße muss entweder 0 bis 1 sein." << std::endl;
         return -1;
     }
@@ -141,14 +141,14 @@ int main(int argc, char** argv){
         read_write = false;     
     }
 
-    if (int_key_same == 1){
-        key_same = true;
+    if (int_value_same == 1){
+        value_same = true;
     }else{
-        key_same = false;     
+        value_same = false;     
     }
 
     if (read_write == true){
-        writeFile<uint32_t,uint32_t>(file_name, key_num, key_same);
+        writeFile<uint32_t,uint32_t>(file_name, key_num, value_same);
     }else{
         readFile<uint32_t,uint32_t>(file_name, key_num);
     }
